726a9ea80d7e53341f0665e03f7beee71d8372c6
Ri <rmerritt6@student.gsu.edu>
2024-10-03 09:21:17 -0400

    Fixed syntax issues

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Task {
   String name;
   bool isCompleted;
 
-  Task({required this.name, this.isCompleted = false})
+  Task({required this.name, this.isCompleted = false});
 }
 
 class TaskListScreen extends StatefulWidget {
@@ -40,79 +40,72 @@ class _TaskListScreenState extends State<TaskListScreen> {
     });
   }
 
-  void_toggleTaskCompletion(int index) {
+  void _toggleTaskCompletion(int index) {
     setState(() {
       _tasks[index].isCompleted = !_tasks[index].isCompleted;
     });
   }
 
-  void _toggleTaskCompletion(int index){
-    setState((){
-      _tasks[index].isCompleted = !_tasks[index].isCompleted;
-    });
-  }
-
-  void_deleteTask(int index){
+  void _deleteTask(int index) {
     setState(() {
       _tasks.removeAt(index);
     });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-  appBar: AppBar(
-    title: Text('Task Manager'),
-  ),
-  body: Column(
-    children [
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children:[
-           Expanded(
-            child:TextField(
-              controller: _taskController,
-              decoration: InputDecoration(
-               labelText: 'Enter task name', 
-              ),
-            ),
-           ),
-           IconButton(
-            icon: Icon(Icons.add),
-            onPressed:_addTask,
-           ), 
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Task Manager'),
       ),
-      Expanded(
-        child: ListView.builder(
-          itemCount: _tasks.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: Checkbox(
-                value: _tasks[index].isCompleted,
-                onChanged:(_)=> _toggleTaskCompletion(index),
-              ),
-              title: Text(
-                _tasks[index].name,
-                style: TextStyle(
-                  decoration: _tasks[index].isCompleted
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _taskController,
+                    decoration: InputDecoration(
+                      labelText: 'Enter task name',
+                    ),
+                  ),
                 ),
-              ),
-              trailing: IconButton(
-                icon: Icon(Icons.delete),
-                onPressed: () => _deleteTask(index),
-              ),
-            );
-          },
-        ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: _addTask,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Checkbox(
+                    value: _tasks[index].isCompleted,
+                    onChanged: (_) => _toggleTaskCompletion(index),
+                  ),
+                  title: Text(
+                    _tasks[index].name,
+                    style: TextStyle(
+                      decoration: _tasks[index].isCompleted
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                    ),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () => _deleteTask(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-    ],
-
-  ),
     );
   }
 }
